c3cb5e8daa7570f5d7ac58834e6c07b2a4e99277
ITSOK\anonm <dev1ender@gmail.com>
2017-05-31 20:45:52 +0530

    final

---
+++ AsignThreeTwo.java
@@ -0,0 +1,11 @@
+public class AsignThreeTwo {
+	int x; int y;
+	String name;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		AsignThreeTwo pnt = new AsignThreeTwo();
+		System.out.println("Pnt is"+pnt.name+""+pnt.x+""+pnt.y);
+		
+	}
+
+}
